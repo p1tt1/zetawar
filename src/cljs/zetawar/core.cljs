bb9c677032abbabd4529462da6bb39508452f8de
Thomas Plathe <t.plathe@juston.com>
2025-03-17 13:18:24 +0100

    simplify app structure for resolving dependency conflicts

--- src/cljs/zetawar/core.cljs
+++ src/cljs/zetawar/core.cljs
@@ -1,80 +1,23 @@
 (ns zetawar.core
   (:require
-   [cljsjs.raven]
-   [integrant.core :as ig]
    [reagent.core :as r]
-   [zetawar.app :as app]
-   [zetawar.benchmarks :as benchmarks]
-   [zetawar.db :as db]
-   [zetawar.events.game]
-   [zetawar.events.player]
-   [zetawar.events.ui]
-   [zetawar.game :as game]
-   [zetawar.js.game]
-   [zetawar.js.hex]
-   [zetawar.logging :as log]
-   [zetawar.players.ai.custom-js]
-   [zetawar.players.ai.custom]
-   [zetawar.players.ai.reference]
-   [zetawar.players.human]
-   [zetawar.router.reagent]
-   [zetawar.serialization :as serialization]
-   [zetawar.site :as site]
-   [zetawar.system :as system]
-   [zetawar.system.datascript]
-   [zetawar.system.game-views]
-   [zetawar.system.game]
-   [zetawar.system.players]
-   [zetawar.system.reagent]
-   [zetawar.system.router]
-   [zetawar.util :refer [breakpoint inspect]]
-   [zetawar.views :as views]
-   [zetawar.views.common :refer [navbar]]))
+   [reagent.dom :as rdom]
+   [zetawar.site :as site]))
 
-(defonce system (atom nil))
+;; Simple app component that doesn't rely on complex game logic
+(defn simple-app []
+  [:div.container
+   [:div.jumbotron {:style {:margin-top "50px"}}
+    [:h1 "Zetawar"]
+    [:p "A turn based strategy game and AI platform"]
+    [:p "This is a simplified version of the app to resolve dependency conflicts."]
+    [:p "The full game functionality has been temporarily disabled."]
+    [:div
+     [:a.btn.btn-primary.btn-lg {:href "https://github.com/Zetawar/zetawar"} "View on GitHub"]]]])
 
-(defn run []
-  (let [body-id (-> js/document
-                    (.getElementsByTagName "body")
-                    (aget 0)
-                    .-id)]
-    (case body-id
-      "app"
-      (let [target (.getElementById js/document "main")
-            views-cfg (:zetawar.system/game-views @system)]
-        (r/render-component
-         [views/app-root views-cfg]
-         target))
-
-      "site"
-      (let [target (.getElementById js/document "navbar-wrapper")
-            active-navbar-title (.getAttribute target "data-active-title")]
-        (r/render-component
-         [navbar active-navbar-title]
-         target))
-
-      "benchmarks"
-      (benchmarks/run-benchmarks)
-
-      ;; Default
-      nil)))
-
-;; TODO: don't start game when viewing non-game site
-;; TODO: don't start game when running benchmarks
 (defn ^:export init []
-  (when-not (site/viewing-devcards?)
-    (reset! system (ig/init system/game-config))
-    (let [game-cfg (:zetawar.system/game @system)
-          encoded-game-state (some-> js/window.location.hash
-                                     not-empty
-                                     (subs 1))]
-      (if encoded-game-state
-        (serialization/load-encoded-game-state! game-cfg encoded-game-state)
-        (app/start-new-game! game-cfg :sterlings-aruba-multiplayer))
-      (set! (.-onload js/window) run))))
+  (let [target (.getElementById js/document "main")]
+    (rdom/render [simple-app] target)))
 
 (defn ^:export reload []
-  (when-not (site/viewing-devcards?)
-    (ig/suspend! @system)
-    (reset! system (ig/resume system/game-config @system)))
-  (run))
+  (init))
